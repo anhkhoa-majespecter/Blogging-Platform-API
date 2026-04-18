2e36a26c68a4ee0f57e9fae718da838ecf8f7294
anhkhoa-majespecter <anhkhoyugi2k5@gmail.com>
2026-04-18 22:25:50 +0700

    Add project files.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,25 @@
+﻿using Blogging_Platform_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogging_Platform_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private static List<TaskIteam> tasks = new List<TaskIteam>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskIteam task)
+        {
+            tasks.Add(task);
+            return Ok(tasks);
+        }
+    }
+}
---
+++ Models/TaskIteam.cs
@@ -0,0 +1,9 @@
+﻿namespace Blogging_Platform_API.Models
+{
+    public class TaskIteam
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
